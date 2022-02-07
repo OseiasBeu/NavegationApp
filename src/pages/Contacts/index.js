04ced1c64056eb7533bb89283c46ae303a68206c
Oseias Beu <oseiasbeu@outlook.com>
2022-02-07 13:22:10 -0300

    Criar navegação stack passando parametros sobre informações dos contatos

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -4,11 +4,41 @@ import {View, Text } from 'react-native'
 export default function Contacts({navigation}){
     return(
         <View>
-            <Text>João</Text>
-            <Text
-            onPress={()=> navigation.navigate('Information')}
-            >Information...</Text>
+            <View style={{marginTop:60}}>
+                <Text>Nome: João Silva</Text>
+                <Text>Telefone: (11) 5533-5565</Text>
+                <Text
+                onPress={()=> navigation.navigate('Information',
+                {
+                    nome: 'João Silva',
+                    telefone:'(11) 5533-5565',
+                    endereco: 'rua das flores',
+                    numero:198,
+                    profissao:'carpinteiro',
+                    email:'JoaoSilva@carpintaria.com.br'
+                }
+                )}
+                >Information...
+                </Text>
+            </View>
 
+            <View style={{marginTop:60}}>
+                <Text>Nome: Oseias Beu</Text>
+                <Text>Telefone: (11) 5633-7652</Text>
+                <Text
+                onPress={()=> navigation.navigate('Information',
+                {
+                    nome: 'Oseias Beu',
+                    telefone:' (11) 5633-7652',
+                    endereco: 'rua das neves',
+                    numero:192,
+                    profissao:'carpinteiro',
+                    email:'beuoseias@gmail.com'
+                }
+                )}
+                >Information...
+                </Text>
+            </View>
         </View>
     )
 }
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {View, Text } from 'react-native'
 
-export default function Information(){
+export default function Information({ route }){
     return(
-        <View>
-            <Text>
-                Information
-            </Text>
+        <View style={{marginTop:60}}><Text>Information</Text>
+            <Text>Nome: {route.params?.nome}</Text>
+            <Text>Telefone: {route.params?.telefone}</Text>
+            <Text>Endereço: {route.params?.endereco}</Text>
+            <Text>Nº: {route.params?.numero}</Text>
+            <Text> Profissão: {route.params?.profissao}</Text>
+            <Text>Email: {route.params?.email}</Text>
         </View>
     )
 }
